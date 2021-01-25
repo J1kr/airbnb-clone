f71bc8d2d10ed580d285dd7b6308239bf4a926b0
mogki <mogki@naver.com>
2021-01-25 16:41:23 +0900

    #24.8 Booked Day

--- config/settings.py
+++ config/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+
         },
     },
 ]
@@ -142,8 +143,6 @@ EMAIL_PORT = "587"
 EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
 EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
 EMAIL_FROM = "j1@mogki.com"
-
-
 
 # Auth
 
