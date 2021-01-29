0e74a7262aa266c1be8de32a2c4489810f27dba4
mogki <mogki@naver.com>
2021-01-29 15:49:53 +0900

    #26.4 container_commands 0.0041

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET")
 # DEBUG = bool(os.environ.get("DEBUG"))
 DEBUG = True
 
-ALLOWED_HOSTS = ["mogki.com", "www.mogki.com"]
+ALLOWED_HOSTS = ["mogki.com", "www.mogki.com" ".elasticbeanstalk.com"]
 
 
 # Application definition
