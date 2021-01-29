4a2b50873421a09c414183778d942652580566dd
mogki <mogki@naver.com>
2021-01-29 13:16:25 +0900

    #26.3 Postgres part Two - 0.003

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG"))
+# DEBUG = bool(os.environ.get("DEBUG"))
+DEBUG = True
 
 ALLOWED_HOSTS = ["mogki.com", "www.mogki.com"]
 
